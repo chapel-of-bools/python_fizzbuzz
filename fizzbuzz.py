086d75db694cf32a5d3914b1fa43324e31e00d7e
Matthew Scott <matthewruelscott@gmail.com>
2016-07-29 09:35:44 -0400

    4 solutions to FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,20 +1,7 @@
 # SOLUTION 1
-count = 0
-while count < 101:
-    if count % 5 == 0 and count %3 == 0:
-        print "Fizzbuzz"
-    elif count % 3 == 0:
-        print "Fizz"
-    elif count % 5 == 0:
-        print "Buzz"
-    else:
-        print count
-
-    count = count + 1
-
-# SOLUTION 2
-for num in xrange(1,101):
-    if num % 5 == 0 and num % 3 == 0:
+num = 0
+while num < 101:
+    if num % 5 == 0 and num %3 == 0:
         print "Fizzbuzz"
     elif num % 3 == 0:
         print "Fizz"
@@ -23,22 +10,35 @@ for num in xrange(1,101):
     else:
         print num
 
-# SOLUTION 3
-for num in xrange(1,101):
-    msg = ""
-    if num % 3 == 0:
-        msg += "Fizz"
-    if num % 5 == 0:
-        msg += "Buzz"
-    if not msg:
-        msg += str(num)
-    print msg
-
-# SOLUTION 4
-for num in xrange(1,101):
-    msg = ""
-    if num % 3 == 0:
-        msg += "Fizz"
-    if num % 5 == 0:
-        msg += "Buzz"
-    print msg or num
+    num = num + 1
+#
+# # SOLUTION 2
+# for num in xrange(1,101):
+#     if num % 5 == 0 and num % 3 == 0:
+#         print "Fizzbuzz"
+#     elif num % 3 == 0:
+#         print "Fizz"
+#     elif num % 5 == 0:
+#         print "Buzz"
+#     else:
+#         print num
+#
+# # SOLUTION 3
+# for num in xrange(1,101):
+#     msg = ""
+#     if num % 3 == 0:
+#         msg += "Fizz"
+#     if num % 5 == 0:
+#         msg += "Buzz"
+#     if not msg:
+#         msg += str(num)
+#     print msg
+#
+# # SOLUTION 4
+# for num in xrange(1,101):
+#     msg = ""
+#     if num % 3 == 0:
+#         msg += "Fizz"
+#     if num % 5 == 0:
+#         msg += "Buzz"
+#     print msg or num
