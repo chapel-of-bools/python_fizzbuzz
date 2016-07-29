112df66b874f982bdd2cd4f488bd7b003d624b54
Matthew Scott <matthewruelscott@gmail.com>
2016-07-29 09:21:17 -0400

    4 solutions to FizzBuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,44 @@
+# SOLUTION 1
+count = 0
+while count < 101:
+    if count % 5 == 0 and count %3 == 0:
+        print "Fizzbuzz"
+    elif count % 3 == 0:
+        print "Fizz"
+    elif count % 5 == 0:
+        print "Buzz"
+    else:
+        print count
+
+    count = count + 1
+
+# SOLUTION 2
+for num in xrange(1,101):
+    if num % 5 == 0 and num % 3 == 0:
+        print "Fizzbuzz"
+    elif num % 3 == 0:
+        print "Fizz"
+    elif num % 5 == 0:
+        print "Buzz"
+    else:
+        print num
+
+SOLUTION 3
+for num in xrange(1,101):
+    msg = ""
+    if num % 3 == 0:
+        msg += "Fizz"
+    if num % 5 == 0:
+        msg += "Buzz"
+    if not msg:
+        msg += str(num)
+    print msg
+
+# SOLUTION 4
+for num in xrange(1,101):
+    msg = ""
+    if num % 3 == 0:
+        msg += "Fizz"
+    if num % 5 == 0:
+        msg += "Buzz"
+    print msg or num
